188b3d670d75d31391fbd733f1d55b16569a5750
Markus Schroefl <markus.schroefl@gmx.at>
2023-12-02 20:51:30 +0100

    Day01Prob2

---
+++ Markus/Day01/Problem 2/functions.h
@@ -0,0 +1,53 @@
+#include<string>
+#include<vector>
+#include<iostream>
+
+inline bool IsNumber(char ch) {return (ch>47)&&(ch<58);}
+int ConvertToNumber(char);
+void ConvertLine (std::string&);
+
+
+// Implement functions
+
+int ConvertToNumber(char ch)
+{
+    if (!IsNumber(ch)) {return -1;}
+    return ch-48;
+}
+
+// list of string digits
+std::vector<std::string> stringDigits {"one","two","three","four","five","six","seven","eight","nine"};
+
+// so unfassbar haesslich ...
+void ConvertLine (std::string& line)
+{
+    for (std::string::size_type i = 0; i < line.size(); i++)
+    {
+        switch (line[i])
+        {
+        case 'o':
+            if (line.size()-i>=3 && line[i+1]=='n' && line[i+2]=='e') {line[i] = '1';}
+            break;
+        case 't':
+            if (line.size()-i>=3 && line[i+1]=='w' && line[i+2]=='o') {line[i] = '2';}
+            if (line.size()-i>=5 && line[i+1]=='h' && line[i+2]=='r' && line[i+3]=='e' && line[i+4]=='e') {line[i] = '3';}
+            break;
+        case 'f':
+            if (line.size()-i>=4 && line[i+1]=='o' && line[i+2]=='u' && line[i+3]=='r') {line[i] = '4';}
+            if (line.size()-i>=4 && line[i+1]=='i' && line[i+2]=='v' && line[i+3]=='e') {line[i] = '5';}
+            break;
+        case 's':
+            if (line.size()-i>=3 && line[i+1]=='i' && line[i+2]=='x') {line[i] = '6';}
+            if (line.size()-i>=5 && line[i+1]=='e' && line[i+2]=='v' && line[i+3]=='e' && line[i+4]=='n') {line[i] = '7';}
+            break;
+        case 'e':
+            if (line.size()-i>=5 && line[i+1]=='i' && line[i+2]=='g' && line[i+3]=='h' && line[i+4]=='t') {line[i] = '8';}
+            break;
+        case 'n':
+            if (line.size()-i>=4 && line[i+1]=='i' && line[i+2]=='n' && line[i+3]=='e') {line[i] = '9';}
+            break;
+        default:
+            break;
+        }
+    }
+}
---
+++ Markus/Day01/Problem 2/main.cpp
@@ -0,0 +1,60 @@
+#include"functions.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+
+int main(int argc, char const *argv[])
+{
+    // Read input file
+    std::ifstream inputFile;
+    inputFile.open("day_1_input.txt",std::ios_base::in);
+    if (!inputFile.is_open())
+    {
+        std::cout << "File could not be opened :C\n";
+        exit(1);
+    }
+    std::cout << "Successfully opened file!\n";
+
+    // iterate over imput file
+    int counter {0};
+    int firstDigit {};
+    int lastDigit {};
+    bool isFirstNumber {};
+    std::string dummyLine {};
+    while (std::getline(inputFile, dummyLine))
+    {
+        // Convert line
+        ConvertLine(dummyLine);
+
+        firstDigit = 0;
+        lastDigit = 0;
+        isFirstNumber = true;
+        for (char ch : dummyLine)
+        {
+            if (IsNumber(ch))
+            {
+                lastDigit = ConvertToNumber(ch);
+                if (isFirstNumber)
+                {
+                    firstDigit = ConvertToNumber(ch);
+                    isFirstNumber = false;
+                }
+                
+            }
+        }
+        counter += firstDigit*10 + lastDigit;
+    }
+
+    // print result
+    std::cout << "The result of the task is: " << counter << '\n';
+
+    /* std::string test {"onetwooththreeifourththtfivegnjseventninieninenowsixsi"};
+    ConvertLine(test);
+    std::cout << test << '\n';
+ */
+    inputFile.close();
+
+    return 0;
+}
